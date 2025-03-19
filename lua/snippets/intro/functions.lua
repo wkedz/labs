0ac3a3fac3b6bfacaf7be396e626d7dde97de3b6
Wiesław Kędzierski <w.m.kedzierski@gmail.com>
2025-03-19 22:37:04 +0100

    Add lua related projects to lab repository.

---
+++ lua/snippets/intro/functions.lua
@@ -0,0 +1,10 @@
+-- Functions
+--
+local c , d = 1,2
+local function add ( a, b)
+    return a+b
+end
+print("add ", add(2,2))
+
+local function add2 (a, b) return a+b end
+print("add2 ", add2(1,2))
